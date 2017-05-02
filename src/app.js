2e8818b4419417c688a4a9232f414ffd6540b3ac
won <wjin0352@gmail.com>
2017-05-02 14:09:37 -0400

    created dir for common components from other app to use here, added index file for easy importing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="AUTH"/>
+        <Text>Oh yea!</Text>
       </View>
     );
   }
 }
 
-export default App;
+export default App; 
