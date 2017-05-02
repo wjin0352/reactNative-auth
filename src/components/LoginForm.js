1c0001be7b58851f22f9f9fdd5a0b2c785941fdb
won <wjin0352@gmail.com>
2017-05-02 19:36:32 -0400

    login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection> 
           <Input 
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
         <CardSection />
-
+          <Input 
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
         <CardSection> 
           <Button>
             Log in
